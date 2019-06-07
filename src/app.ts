549b1b2b7f2417bc15f95c17bd2f4db6d8dd5a3c
olzzon <github@olzzon.dk>
2019-06-07 09:39:45 +0200

    initial: Adding CasparCG connection and load a scene

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,25 @@ import net from 'net';
 import { CasparCG } from 'casparcg-connection';
 
 export class App {
+    ccgConnection: CasparCG;
+    vizEngine: any;
+
     constructor() {
         const VIZ_SERVER_PORT: number = 6100;
-        const CCG_SERVER_IP: string = "127.0.0.1"
-        const CCG_SERVER_PORT: number = 5250;
+        const CCG_IP: string = "127.0.0.1"
+        const CCG_PORT: number = 5250;
         let vizMessage: Array<string>;
 
-        const ccgConnection = new CasparCG( CCG_SERVER_IP, CCG_SERVER_PORT);
+        this.ccgConnection = new CasparCG(
+            {
+                host: CCG_IP,
+                port: CCG_PORT,
+                autoConnect: true,
+            }
+        );
 
-        const vizEngine: any = net.createServer((socket) => {
-
+        this.vizEngine = net.createServer((socket) => {
+            const _this2 = this
             console.log("connected");
 
             socket.on("data", function(data) {
@@ -19,6 +28,11 @@ export class App {
                 if (vizMessage[0] === "SCENE") {
                     socket.write('SCENE loading' + '\0');
                     // Load scene in CasparCG
+                    _this2.ccgConnection.play(
+                        1, // output,
+                        20, // layer,
+                        vizMessage[2], //Mediafile
+                    );
                 } else {
                     console.log("Command not implemented :", vizMessage[0]);
                     console.log('++++++++++++++++++++++++++++');
